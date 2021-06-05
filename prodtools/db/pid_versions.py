f56cfb39b60ed069fd8f62077bbe9d8c195ee161
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-06-04 23:29:36 -0300

    Cria atributo `PIDVersionsManager.manage` para gerir os pids de um artigo

--- prodtools/db/pid_versions.py
+++ prodtools/db/pid_versions.py
@@ -65,5 +65,119 @@ class PIDVersionsManager:
         self.session = self.Session()
         return self.session.query(PidVersion).filter_by(v2=v2, v3=v3).count() == 1
 
+    def manage(self, v2, v3, prev, v3_gen):
+        self.session = self.Session()
+        q = self.session.query(PidVersion)
+        ret = (
+            self._search_by_v3(q, v2, v3, prev) or
+            self._search_by_v2_and_prev(q, v2, prev, v3_gen)
+        )
+        if not ret:
+            return None
+        try:
+            self.session.commit()
+        except Exception as e:
+            logging.debug("%s" % str(e))
+            self.session.rollback()
+        else:
+            return ret
+
+    def _search_by_v3(self, q, v2, v3, prev):
+        """
+        
+        """
+        # obtém os registros em que há v3 igual a v3
+        v3_records = [] or v3 and q.filter_by(v3=v3).all()
+        if v3_records:
+            for rec in v3_records:
+                # apaga os registros
+                self.session.delete(rec)
+            # registra os pares (v2, v3) e (prev, v3), se existir
+            for pid_v2 in (v2, prev):
+                if pid_v2:
+                    # adicionar registros com o par v2, v3
+                    self.session.add(PidVersion(v2=pid_v2, v3=v3))
+            # finaliza
+            return (v2, v3, prev)
+
+    def _search_by_v2_and_prev(self, q, v2, prev, v3_gen):
+        # obtém os registros em que há v2 igual a prev
+        prev_records = [] or prev and q.filter_by(v2=prev).all()
+
+        # obtém os registros em que há v2 igual a v2
+        v2_records = [] or v2 and q.filter_by(v2=v2).all()
+
+        v3_values = set([record.v3 for record in prev_records + v2_records])
+
+        return (
+            self._search_by_v2_and_prev__no_record_found(
+                v2, prev, v3_values, v3_gen) or
+            self._search_by_v2_and_prev__one_v3_found(
+                v2, prev, v3_values, v2_records, prev_records) or
+            self._search_by_first_of_prev_or_v2(
+                q, v2, prev, v2_records, prev_records)
+        )
+
+    def _search_by_v2_and_prev__no_record_found(
+            self, v2, prev, v3_values, v3_gen,
+            ):
+        if not v3_values:
+            # nenhum registro encontrado
+            # registra os pares (v2, v3) e (prev, v3), se existir
+            v3 = v3_gen()
+            for pid_v2 in (v2, prev):
+                if pid_v2:
+                    # adicionar registros com o par v2, v3
+                    self.session.add(PidVersion(v2=pid_v2, v3=v3))
+            # finaliza
+            return (v2, v3, prev)
+
+    def _search_by_v2_and_prev__one_v3_found(
+            self, v2, prev, v3_values, v2_records, prev_records):
+        if len(v3_values) == 1:
+            # pelo menos 1 registro encontrado, de prev ou de v2 ou de ambos
+            if len(prev_records) == len(v2_records) == 1:
+                # ambos
+                # finaliza
+                return (v2, None, prev)
+
+            # apaga os registros excedentes, se existirem
+            self._remove_exceding_records((prev_records[1:], v2_records[1:]))
+
+            # cria registro, se não existir
+            v3 = v3_values.pop()
+            if prev and len(prev_records) == 0:
+                # adicionar registro com o par (prev, v3), se não existir
+                self.session.add(PidVersion(v2=prev, v3=v3))
+            if v2 and len(v2_records) == 0:
+                # adicionar registro com o par (v2, v3), se não existir
+                self.session.add(PidVersion(v2=v2, v3=v3))
+            # finaliza
+            return (v2, v3, prev)
+
+    def _search_by_first_of_prev_or_v2(
+            self, q, v2, prev, v2_records, prev_records):
+        """
+        Obtém o primeiro registro,
+        considerando a sequencia de prioridade prev e v2
+        """
+        record = (
+            prev and q.filter_by(v2=prev).first() or
+            v2 and q.filter_by(v2=v2).first()
+        )
+        if record:
+            self._remove_exceding_records((prev_records, v2_records), [record])
+            return (v2, record.v3, prev)
+
+    def _remove_exceding_records(self, group_of_records, valid_records=None):
+        """
+        Apaga os registros excedentes, se existirem
+        """
+        valid_records = valid_records or []
+        for records in group_of_records:
+            for rec in records:
+                if rec not in valid_records:
+                    self.session.delete(rec)
+
     def close(self):
         self.__exit__()
